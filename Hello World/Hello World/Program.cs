ba28ba8649221b6c705845214e376ea5e555390e
mohamed qutaish <mooohmedshafie1234@gmail.com>
2024-08-07 21:43:14 -0400

    My code with the change

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -10,8 +10,45 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wrold");
-            Console.Read();
+          
+           
         }
     }
 }
+
+
+
+
+
+//using string and it events
+//string myname = "Moe";
+//string message = "My name is " + myname;
+//string CapMessage = message.ToUpper();
+
+//string lowerCaseMessage = message.ToLower();
+
+//Console.WriteLine(lowerCaseMessage);
+//Console.Read();
+
+
+
+
+
+
+
+
+//Calculating:
+//int num1 = 13;
+//int num2 = 14;
+//int sum = num1 + num2;
+
+//double d1 = 3.5;
+//double d2 = 1.337;
+//double sumD = d1 + d2;
+
+//float f1 = 3.5f;
+//float f2 = 5.4f;
+//float sumf = f1 + f2;
+//Console.WriteLine("the sum of " + num1 + " and " + num2 + " is " + sum);
+//Console.WriteLine("the sum of " + d1 + " and " + d2 + " is " + sumD);
+//Console.WriteLine("the sum of " + f1 + " and " + f2 + " is " + sumf);
