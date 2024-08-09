84cc715ae34d3975534c2d00dc7d6d8f5d991ff5
mohamed qutaish <mooohmedshafie1234@gmail.com>
2024-08-08 21:41:57 -0400

    the Conversion and Parse codes

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace Hello_World
 {
-    internal class Program
+     class Program
     {
         static void Main(string[] args)
         {
-          
-           
+            
+
+
+
         }
     }
 }
@@ -19,6 +21,48 @@ namespace Hello_World
 
 
 
+
+// getting a int out of string or double out of string the same process
+//string myString = "15";
+//string mySecondString = "13";
+//int num1 = Int32.Parse(myString);
+//int num2 = Int32.Parse(mySecondString);
+//int resultInt = num1 + num2;
+//string result = myString + mySecondString;
+
+//Console.WriteLine(resultInt);
+//Console.Read();
+
+
+
+
+
+
+
+// CONVERSION
+//imicit conversion
+// int num = 12390532;
+//long bigNum = num;
+
+//float myFloat = 13.37f;
+// double myNewDouble = myFloat;
+
+
+// double myDouble = 13.37;
+// int myInt;
+//explicit conversion
+//cast double into int;
+// myInt = (int)myDouble;
+
+// typeConversion, changing float to string adding .tostting;
+//string myString = myDouble.ToString(); // "13.37"
+//string myFloatString = myFloat.ToString();
+//bool sunIsShining = false;
+
+//string myBoolString = sunIsShining.ToString();
+
+//Console.WriteLine(myBoolString);
+//Console.Read();
 
 //using string and it events
 //string myname = "Moe";
